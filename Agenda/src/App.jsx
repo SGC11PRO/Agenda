7e0d8f6c319e64473b8903424533af6979cf7bab
Sergio G <sergio.garcia.cristobal@gmail.com>
2024-08-23 19:29:15 +0200

    Toggle Important Notes

--- Agenda/src/App.jsx
+++ Agenda/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   // almacenar las notas en un estado del componente
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
 
   // funcion para añadir nueva nota
   const addNote = (event) => {
@@ -16,7 +17,7 @@ const App = () => {
     const noteObject = {
       content: newNote,
       id: notes.length + 1,
-      important: Math.random > 0.5
+      important: Math.random() > 0.5
     }
 
     // actualizamos el array de notas
@@ -30,9 +31,19 @@ const App = () => {
 
   }
 
+  // funcion para registrar los cambios de texto en el input
   const handleNoteChange = (event) => {
 
     setNewNote(event.target.value) // sincroniza el contenido del input, con el valor de la newNote
+  }
+
+
+  // ahora tenemos una nueva variable que cambia segun el valor de note.important
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true) // if(showAll) -> return notes : else -> return aquellas notas con 'important'
+
+  // funcion para cambiar boton de mostrar notas
+  const handleNotesToShow = () => {
+    setShowAll(!showAll) // alterna el valor de showAll
   }
 
   return (
@@ -40,8 +51,14 @@ const App = () => {
 
       <h1>Notes</h1>
 
+      <div>
+        <button onClick={handleNotesToShow}>
+          Show {showAll ? 'Important Notes' : 'All Notes' }
+        </button>
+      </div>
+
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note}/>
         )}
       </ul>
