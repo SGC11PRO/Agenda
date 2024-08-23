6dc87a2b70a147e956b58337af6c5a69b010d478
Sergio G <sergio.garcia.cristobal@gmail.com>
2024-08-23 20:55:03 +0200

    Agenda Step 1
    Add Contacts & Show Contacts

--- Agenda/src/App.jsx
+++ Agenda/src/App.jsx
@@ -1,41 +1,55 @@
 import { useState } from 'react'
+import Contact from './components/Contact'
 
 const App = () => {
   const [contacts, setContacts] = useState([]) 
   const [newName, setNewName] = useState('')
 
   const handleChange = (event) => {
-    setNewName(event.target.value)
-    console.log(newName)
+
+    setNewName(event.target.value) // recuperar el input
   }
 
-  const addContact = () => {
+  const addContact = (event) => {
+
+    event.preventDefault()
+
     const newContact = {
       id: contacts.length + 1,
       name: newName
     }
 
-    setContacts(contacts.concat(newContact))
+    // añadir al array
+    newContact.name.length === 0 
+      ? console.error('Ingresa un nombre para guardar el contacto') 
+      : setContacts(contacts.concat(newContact))
 
-    console.log(newContact)
+    console.log(contacts)
+
+    // limpiar input
+    setNewName('')
   }
 
   return (
     <div>
       <h2>Contactos</h2>
+      <ul>
+          {contacts.map(note =>
+            <Contact contact={note.name} key={note.id}/>
+          )}
+      </ul>
+
       <form>
 
         <div>
-          Agregar Contacto : <input onChange={handleChange}/>
+          Agregar Contacto : <input onChange={handleChange} value={newName}/>
         </div>
 
         <div>
-          <button >Añadir</button>
+          <button onClick={addContact}>Añadir</button>
         </div>
 
       </form>
-      <h2>Numbers</h2>
-      ...
     </div>
   )
 }
---
+++ Agenda/src/components/Contact.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Contact = ({ contact }) => {
+    return (
+      <li>{contact}</li>
+    )
+  }
+
+  export default Contact
