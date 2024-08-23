ecc8253409822ed31719b2a82b85f86239bf6a01
Sergio G <sergio.garcia.cristobal@gmail.com>
2024-08-23 19:35:26 +0200

    Start doing exercise

--- Agenda/src/App.jsx
+++ Agenda/src/App.jsx
@@ -1,74 +1,42 @@
-import Note from "./components/Note"
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 const App = () => {
+  const [contacts, setContacts] = useState([]) 
+  const [newName, setNewName] = useState('')
 
-  // almacenar las notas en un estado del componente
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+    console.log(newName)
+  }
 
-  // funcion para añadir nueva nota
-  const addNote = (event) => {
-
-    event.preventDefault() // evita recargar la pagina al enviar el evento
-
-    // creamos el objeto de la nota
-    const noteObject = {
-      content: newNote,
-      id: notes.length + 1,
-      important: Math.random() > 0.5
+  const addContact = () => {
+    const newContact = {
+      id: contacts.length + 1,
+      name: newName
     }
 
-    // actualizamos el array de notas
-    setNotes(notes.concat(noteObject))
-    
-    console.log(noteObject)
+    setContacts(contacts.concat(newContact))
 
-    // limpiamos el input field
-    setNewNote('')
-
-
-  }
-
-  // funcion para registrar los cambios de texto en el input
-  const handleNoteChange = (event) => {
-
-    setNewNote(event.target.value) // sincroniza el contenido del input, con el valor de la newNote
-  }
-
-
-  // ahora tenemos una nueva variable que cambia segun el valor de note.important
-  const notesToShow = showAll ? notes : notes.filter(note => note.important === true) // if(showAll) -> return notes : else -> return aquellas notas con 'important'
-
-  // funcion para cambiar boton de mostrar notas
-  const handleNotesToShow = () => {
-    setShowAll(!showAll) // alterna el valor de showAll
+    console.log(newContact)
   }
 
   return (
-    <>
-
-      <h1>Notes</h1>
-
-      <div>
-        <button onClick={handleNotesToShow}>
-          Show {showAll ? 'Important Notes' : 'All Notes' }
-        </button>
-      </div>
-
-      <ul>
-        {notesToShow.map(note => 
-          <Note key={note.id} note={note}/>
-        )}
-      </ul>
-
+    <div>
+      <h2>Contactos</h2>
       <form>
-        <input value={newNote} onChange={handleNoteChange}/>
-        <button onClick={addNote}>Create</button>
-      </form>
 
-    </>
+        <div>
+          Agregar Contacto : <input onChange={handleChange}/>
+        </div>
+
+        <div>
+          <button >Añadir</button>
+        </div>
+
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
   )
 }
 
