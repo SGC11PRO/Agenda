7f5ade465d53f849cf453773aefcb82fd391c069
Sergio G <sergio.garcia.cristobal@gmail.com>
2024-08-23 19:15:42 +0200

    Crear nueva nota

--- Agenda/src/App.jsx
+++ Agenda/src/App.jsx
@@ -1,6 +1,39 @@
 import Note from "./components/Note"
+import { useState, useEffect } from 'react'
 
-const App = ({ notes }) => {
+const App = () => {
+
+  // almacenar las notas en un estado del componente
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+
+  // funcion para añadir nueva nota
+  const addNote = (event) => {
+
+    event.preventDefault() // evita recargar la pagina al enviar el evento
+
+    // creamos el objeto de la nota
+    const noteObject = {
+      content: newNote,
+      id: notes.length + 1,
+      important: Math.random > 0.5
+    }
+
+    // actualizamos el array de notas
+    setNotes(notes.concat(noteObject))
+    
+    console.log(noteObject)
+
+    // limpiamos el input field
+    setNewNote('')
+
+
+  }
+
+  const handleNoteChange = (event) => {
+
+    setNewNote(event.target.value) // sincroniza el contenido del input, con el valor de la newNote
+  }
 
   return (
     <>
@@ -12,6 +45,11 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note}/>
         )}
       </ul>
+
+      <form>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button onClick={addNote}>Create</button>
+      </form>
 
     </>
   )
